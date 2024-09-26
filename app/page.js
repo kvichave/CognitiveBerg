20a092ad232fa1a4c95fa4bda415b1aa9bbfaf02
kunal <kunalvichave258@gmail.com>
2024-09-26 12:02:29 +0530

    just  updated

--- app/page.js
+++ app/page.js
@@ -1,10 +1,10 @@
 import Image from "next/image";
-import Link from "next/link";
-
+import {useRouter} from "next/navigation";
 export default function Home() {
+  const router=useRouter()
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Link href={"/call"}>CALL</Link>
+      <button onClick={() => router.push("/call")}>click here</button>
     </main>
   );
 }
