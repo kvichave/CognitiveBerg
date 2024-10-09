928cecde7ecc3ab9467f9729905546ea74500e97
kunal <kunalvichave258@gmail.com>
2024-10-09 15:16:00 +0530

    clerk added

--- app/page.js
+++ app/page.js
@@ -5,6 +5,7 @@ import { BackgroundLines } from "@/components/ui/background-lines";
 import { FlipWords } from "@/components/ui/flip-words";
 import { LampContainer } from "@/components/ui/lamp";
 import { motion } from "framer-motion";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   const words = [
@@ -18,13 +19,23 @@ export default function Home() {
   return (
     <>
       <BackgroundLines className="flex items-center justify-center w-full flex-col px-4">
-        <h2 className="bg-clip-text text-transparent text-center bg-gradient-to-b from-neutral-900 to-neutral-700 dark:from-neutral-600 dark:to-white text-2xl md:text-4xl lg:text-7xl font-sans py-2 md:py-10 relative z-20 font-bold tracking-tight">
-          <br /> Cognitive Berg.
-        </h2>
-        <p className="max-w-xl mx-auto text-sm md:text-lg text-neutral-700 dark:text-neutral-400 text-center">
-          Transforming Conversations into Confident Connections through
-          Interactive Simulations!
-        </p>
+        <div className="text-4xl z-30 mb-auto ml-auto">
+          <UserButton
+            showName
+            userProfileProps={{
+              className: "text-lg font-semibold text-blue-400", // Customize name styles here
+            }}
+          ></UserButton>
+        </div>
+        <div className="mb-auto">
+          <h2 className="bg-clip-text  text-transparent text-center bg-gradient-to-b from-neutral-900 to-neutral-700 dark:from-neutral-600 dark:to-white text-2xl md:text-4xl lg:text-7xl font-sans py-2 md:py-10 relative z-20 font-bold tracking-tight">
+            <br /> Cognitive Berg.
+          </h2>
+          <p className="max-w-xl   mx-auto text-sm md:text-lg text-neutral-700 dark:text-neutral-400 text-center">
+            Transforming Conversations into Confident Connections through
+            Interactive Simulations!
+          </p>
+        </div>
       </BackgroundLines>
 
       <div className="h-[20rem] flex justify-center items-center px-4">
